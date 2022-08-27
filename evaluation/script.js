206ed2d65bcaed89b46ab64e4864bb06d153a405
Richard Wu <richardwu@Richards-MBP.attlocal.net>
2022-08-27 14:39:44 -0700

    Cleaned up console.log and comments (unnecessary), also the user can now select 0 credits if they want (I'm kind of surprised doing anything with required wasn't a thing for this assignment?)

--- evaluation/script.js
+++ evaluation/script.js
@@ -109,63 +109,20 @@ const Model = ((api, view) => {
 /* ~~~~~~~~~~~~~~~~ Controller ~~~~~~~~~~~~~~~~ */
 const Controller = ((model, view) => {
   const state = new model.State();
-  console.log("hUHHH");
-  console.log(state.todolist);
-  console.log(state.enrolledList);
-
-  console.log("hUHHH");
-
   const selectTodo = () => {
     const todoContainer = document.querySelector(view.domstr.todoContainer);
     todoContainer.addEventListener('click', (event) => {
       var target = event.target;
-      // console.log("WAHH")
-      console.log(target.parentElement.tagName)
       if (target.parentElement.tagName === "LI"){
-        console.log("YAYYAYAY")
         target = target.parentElement
       }
-      console.log(target)
-
-      // if (target.type === span) {
-      //   console.log("SPAN SPAN SPAN");
-      //   console.log("SPAN SPAN SPAN");
-
-      //   console.log("SPAN SPAN SPAN");
-      //   console.log("SPAN SPAN SPAN");
-
-      // }
-      console.log("situational therapy");
-      console.log(state.todolist);
-      console.log("situational therapy");
-      // console.log(event.target.className);
-      // state.todolist = state.todolist.forEach((element) => {
-      //   console.log(element.selected);
-      //   // if (element.selected === false) {
-
-      //   // } else {
-
-      //   // };
-      // });
       if (!target.classList.contains("selected")){
         if (+target.classList[0].substring(8) + +document.getElementById("creditCounter").innerHTML <= 18) {
           target.classList.add("selected");
           document.getElementById("creditCounter").innerHTML = +target.classList[0].substring(8) + +document.getElementById("creditCounter").innerHTML;
-          // const { userId, id, title, completed } = state.todolist.find(x=>x.id === event.target.id);
-          console.log(state.todolist)
-          console.log(+target.id)
-          console.log(state.todolist.find(x=>+x.id === +target.id));
           const { userId, id, title, completed } = (state.todolist.find(x=>+x.id === +target.id));
-          console.log(userId);
           const item = new model.Todo(userId, id, title, completed);
-          console.log(userId);
-          console.log("gonna select");
-          console.log(item);
-          console.log("selected");
           state.selectedlist = ([...state.selectedlist, item]);
-          console.log(state.selectedlist);
-          console.log("selected");
-          // state.selectedList([course, ...state.tempCourses]);
         } else {
           alert("You can only choose up to 18 credits in one semester");
         }
@@ -175,61 +132,13 @@ const Controller = ((model, view) => {
          state.selectedlist = state.selectedlist.filter(
           (item) => +item.id !== +target.id
          );
-         console.log(target.id)
-         console.log("removed hopefully")
-         console.log(state.selectedlist)
-        // state.todolist = state.todolist.filter(
-        //   (todo) => +todo.id !== +event.target.id
-        // );
-        // model.deleteTodo(event.target.id);
       }
-      // console.log(event.target.classList);
-      // console.log(event.target.id);
-      // console.log("egg");
-      // if (event.target.className === 'deletebtn') {
-      //   state.todolist = state.todolist.filter(
-      //     (todo) => +todo.id !== +event.target.id
-      //   );
-      // }
-      // model.deleteTodo(+event.target.id);
     });
-  };
-
-  // const selectTodo = () => {
-  //   console.log("egg");
-  //   const todoContainer = document.querySelector(view.domstr.todoContainer);
-  //   todoContainer.addEventListener('click', (event) => {
-  //     if (event.target.className === "unselected") {
-  //       event.target.className = "selected";
-  //       console.log("urmum");
-  //     }
-  //   });
-  // };
-
-  const addTodo = () => {
-    // const inputbox = document.querySelector(view.domstr.inputebox);
-    // inputbox.addEventListener('keyup', (event) => {
-    //   if (event.key === 'Enter' && event.target.value.trim()) {
-    //     const newTodo = new model.Todo(event.target.value);
-    //     model.addTodo(newTodo).then(todo => {
-    //       state.todolist = [todo, ...state.todolist];
-    //     });
-    //     event.target.value = '';
-    //   }
-    // });
   };
 
   const confirmButton= () => {
     document.getElementById("selectButton").addEventListener("click", function() {
-      // console.log("if it works here then why-")
-      // console.log(state.enrolledList)
       var collection = document.getElementsByClassName("selected")
-      // console.log("wow I got pressed");
-      if (collection.length === 0) {
-        console.log(state.enrolledList)
-        console.log("lol blank selections lmao");
-      } else {
-        console.log(state.enrolledList)
         var creds = document.getElementById('creditCounter').innerHTML;
         let text = "You have chosen " +
           creds +
@@ -237,7 +146,6 @@ const Controller = ((model, view) => {
           if (confirm(text) == true) {
             state.enrolledList = ([...state.enrolledList, ...state.selectedlist]);
             for(var i = 0; i < state.selectedlist.length; i++) {
-              console.log(state.selectedlist[i]);
               state.todolist = state.todolist.filter(
                 (item) => (item.id !== state.selectedlist[i].id)
                );
@@ -247,62 +155,17 @@ const Controller = ((model, view) => {
           } else {
             text = "You canceled!";
           }
-        // alert('You have chosen ${creds} credits for this semester. You cannot change once you submit. Do you want to confirm?')
-        // const coursesToAdd = state.selectedlist;
-        // state.setSelectedCourses(coursesToAdd);
-        // console.log("enrowo");
-        // console.log(state.enrolledList);
-        // state.enrolledList = ([...state.enrolledList, ...state.selectedlist]);
-        // // console.log(state.todolist.filter(
-        // //   todo => (!state.selectedlist.includes(todo))
-        // // ));
-        // // console.log(enrolledList);
-       
-        // // console.log("h:");
-        // //   console.log(collection);
-        // //   console.log("h:");
-        // // var ids= [];
-        // // console.log(state.todolist);
-        // for(var i = 0; i < state.selectedlist.length; i++) {
-        //   console.log(state.selectedlist[i]);
-        //   state.todolist = state.todolist.filter(
-        //     (item) => (item.id !== state.selectedlist[i].id)
-        //    );
-        // }
-        // state.selectedlist = [];
-        // console.log(ids);
-        // for (var i = 0; i < ids.length; i++){
-        //   state.todolist = state.todolist.filter(
-        //     (item) => +item.id !== +ids[i]
-        //    );
-        // }
-        // console.log(state.todolist);
-        // console.log(ids);
-        // for(var i = 0; i < ids.length; i++) {
-        //   console.log(ids[i]);
-        //   state.todolist = state.todolist.filter(
-        //     (todo) => +todo.id !== ids[i]
-        //   );
-        //   model.deleteTodo(ids[i]);
-        //   const element = document.getElementById(ids[i]);
-        //   element.remove();
-        // }
-      }
     })
   };
 
   const init = () => {
     model.getTodos().then((todos) => {
-      // state.todolist = [...todos];
       state.todolist = todos;
-      // console.log(state.todolist)
     });
   };
 
   const bootstrap = () => {
     init();
-    // deleteTodo();
-    addTodo();
     selectTodo();
     confirmButton();
   };
